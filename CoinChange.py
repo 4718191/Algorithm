9fd919b1bbcf05e7e21afc8cf218f91524292463
191 <hth4513@naver.com>
2026-05-05 14:05:58 +0900

    Implement CoinChange function for calculating change
    Fix variable name typo and implement coin counting logic.

---
+++ CoinChange.py
@@ -0,0 +1,19 @@
+# 거스름돈
+
+def CoinChange(W):
+  change = W
+  n500, n100, n50, n10, n1 = 0, 0, 0, 0, 0
+  while change >= 500:
+    n500 += 1
+  while change >= 100:
+    n100 += 1
+  while change >= 50:
+    n50 += 1
+  while change >= 10:
+    n10 += 1
+  while chagne >= 1:
+    n1 += 1
+  return n500 + n100 + n50 + n10 + n1
+
+n = int(input('거스름돈: '))
+print(CoinChange(n))
